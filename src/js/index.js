8c6092ac4874be80efee8f50cc7826907692707f
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-01-28 12:48:15 +0100

    delete: Removed redundant js files and code

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,1 @@
 import "../style/style.scss";
-
-var path = window.location.pathname;
-var page = path.split("/").slice(-2)[0];
-
-if (page === "pappaskamt") {
-  const button = document.querySelector(".generate-joke");
-
-  button.addEventListener("click", () => {
-    import("./generateJoke.js").then(async ({ generateJoke }) => {
-      const paragraphJoke = document.querySelector(".joke");
-      paragraphJoke.innerHTML = await generateJoke();
-    });
-  });
-}
